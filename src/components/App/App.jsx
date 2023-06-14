5434184258392b9a788845fef2619c00ec2a06b2
Dave Nash <davidjnash2@gmail.com>
2023-06-13 21:02:19 -0500

    updates from lecture follow-along

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,38 @@
 
 import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
 
-function App () {
+function App() {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">APIS</h1>
-          <h4><i>APIS</i></h4>
-        </header>
-        <br/>
-      </div>
-    );
-  
+  const [gifs, setGifs] = useState([]);
+
+  const handleClick = () => {
+    axios.get('/gifs')
+      .then((response) => {
+        console.log(response.data.data);
+        setGifs(response.data.data);
+      })
+      .catch((error) => {
+        console.log('Error with client GET', error);
+      })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">APIS</h1>
+        <h4><i>APIS</i></h4>
+      </header>
+
+      <button onClick={handleClick}>GET THEM GIFS</button>
+      {gifs.map((gif, i) => (
+        <img key={i} src={gif.images.original.url}></img>
+      ))}
+      <br />
+    </div>
+  );
+
 }
 
 export default App;
